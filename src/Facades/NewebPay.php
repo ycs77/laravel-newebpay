cfeacd29bf09e06ac348f4c708510acc7befe474
Lucas Yang <yangchenshin77@gmail.com>
2020-05-11 15:45:15 +0800

    Add facade method phpdoc

--- src/Facades/NewebPay.php
+++ src/Facades/NewebPay.php
@@ -5,6 +5,15 @@ namespace Ycs77\NewebPay\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Ycs77\NewebPay\NewebPayMPG payment(string $no, int $amt, string $desc, string $email) 付款
+ * @method static \Ycs77\NewebPay\NewebPayCancel creditCancel(string $no, int $amt, string $type = 'order')
+ * @method static \Ycs77\NewebPay\NewebPayClose requestPayment(string $no, int $amt, string $type = 'order')
+ * @method static \Ycs77\NewebPay\NewebPayClose requestRefund(string $no, int $amt, string $type = 'order')
+ * @method static \Ycs77\NewebPay\NewebPayQuery query(string $no, int $amt)
+ * @method static \Ycs77\NewebPay\NewebPayCreditCard creditcardFirstTrade(array $data)
+ * @method static \Ycs77\NewebPay\NewebPayCreditCard creditcardTradeWithToken(array $data)
+ * @method static mixed decode(string $encryptString)
+ *
  * @see \Ycs77\NewebPay\NewebPay
  */
 class NewebPay extends Facade
