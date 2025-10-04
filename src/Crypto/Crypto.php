fea7b14329dfae478276997a38ac39bdacb37e9a
Lucas Yang <yangchenshin77@gmail.com>
2025-10-04 23:48:42 +0800

    Tweak

--- src/Crypto/Crypto.php
+++ src/Crypto/Crypto.php
@@ -35,9 +35,7 @@ class Crypto
             throw new EncryptException('加密錯誤');
         }
 
-        $encryptedPostData = trim(bin2hex($value));
-
-        return $encryptedPostData;
+        return trim(bin2hex($value));
     }
 
     /**
@@ -82,9 +80,8 @@ class Crypto
     {
         $len = strlen($string);
         $pad = $blocksize - ($len % $blocksize);
-        $string .= str_repeat(chr($pad), $pad);
 
-        return $string;
+        return $string.str_repeat(chr($pad), $pad);
     }
 
     protected function removePadding(string $string): string
